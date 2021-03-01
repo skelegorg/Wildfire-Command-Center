ca34817ae03baa0f7254741c413d24e1ba21152d
skelegorg <62708878+skelegorg@users.noreply.github.com>
2021-02-28 21:16:40 -0500

    connects to db

--- Back End/app.js
+++ Back End/app.js
@@ -1,7 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
+mongoose.connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => {console.log("Database Connected")});
+mongoose.connection.on("error", err => console.log(`Database Connection Error: ${err.message}`));
