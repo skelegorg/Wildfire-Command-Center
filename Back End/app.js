7906d5f033a81c7e6ce0fd6298104e677b683628
skelegorg <62708878+skelegorg@users.noreply.github.com>
2021-03-02 17:11:09 -0500

    index site appears as intended

--- Back End/app.js
+++ Back End/app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const app = express();
+app.use(express.static("public"));
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true}).then(() => console.log("DB Connected"));
 
+// routes
 const accRoutes = require("./routes/account");
 
+// middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-//app.use("/", (req, res) => {res.send("hi there")});
+app.use(expressValidator())
 app.use("/account", accRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
-})
+});
