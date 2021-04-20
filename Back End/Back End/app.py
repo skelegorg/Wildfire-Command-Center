ff65fc034258dc6716c9323feb6acd00cbad71de
skelegorg <62708878+skelegorg@users.noreply.github.com>
2021-04-19 23:10:02 -0400

    configs added, folder structure established

--- Back End/Back End/app.py
+++ Back End/Back End/app.py
@@ -2,9 +2,20 @@
 
 import flask
 from flask import request, jsonify
+import dotenv
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+# testing purposes
+retTest = [
+    {
+        "e" : "e"
+    }, {
+        'id': 0
+    }
+]
 
 @app.route('/', methods=['GET'])
 def home():
@@ -13,5 +24,14 @@ def home():
 @app.route('/login/<username>', methods=['GET'])
 def login(username):
     return f"<h1> Hello, {username}!</h1>"
+
+@app.route('/data/e', methods=['GET'])
+def ret():
+    return jsonify(retTest)
+
+# error handlers
+@app.errorhandler(404)
+def pageNotFound(e):
+    return "<h1>The page you were looking for was not found.</h1>", 404
 
 app.run()
