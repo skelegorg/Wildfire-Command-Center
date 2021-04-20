3c729592efa3869b3dd2c0ff1d622be2689f5c33
skelegorg <62708878+skelegorg@users.noreply.github.com>
2021-04-19 21:30:35 -0400

    moved public assets and pages to the new dir

--- Back End/Back End/app.py
+++ Back End/Back End/app.py
@@ -1,1 +1,17 @@
 # Wildfire Command Center Deep API
+
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1> Lets try this out</h1>"
+
+@app.route('/login/<username>', methods=['GET'])
+def login(username):
+    return f"<h1> Hello, {username}!</h1>"
+
+app.run()
